7fb07ab8194bc98253381bd32c2b499fb59eeed0
pjroggy <pierrejeanroggy@gmail.com>
2024-11-27 16:27:05 +0100

    feat/api_data/: Create CreditDebit Entity And Resolver

---
+++ api_data/src/creditDebit/creditDebit.entity.ts
@@ -0,0 +1,28 @@
+import { IsNotEmpty, IsString, Length } from "class-validator";
+import { Field, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  // OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class CreditDebit extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => String)
+  @Column({ nullable: false, unique: true, type: "varchar", width: 30 })
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 30)
+  label: string;
+
+  // @Field(() => [Invoice])
+  // @OneToMany(() => Invoice, (invoice) => invoice.creditDebit)
+  // invoices: Invoice[];
+}
