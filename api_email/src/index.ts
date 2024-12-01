f9c65304f13ecfeeb1473dc6e423dc8f6b6f0c80
Fabien Amann <fabien@feub.net>
2024-12-01 12:38:05 +0100

    feat/api_email: sending an HTML email via the /send-email POST endpoint works

--- api_email/src/index.ts
+++ api_email/src/index.ts
@@ -1,12 +1,44 @@
 import * as dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
+import { transporter } from "./utils/transporter";
+import bodyParser from "body-parser";
+// import ejs from "ejs";
 const app = express();
 
-dotenv.config();
-const { PORT } = process.env;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/email", (req, res) => {
+dotenv.config();
+const { PORT, EMAIL_FROM } = process.env;
+
+app.get("/", (_, res: Response) => {
   res.send("Hello Email!");
+});
+
+app.post("/send-email", (req: Request, res: Response) => {
+  const { recipient, subject, text_email, html_email } = req.body;
+
+  transporter.sendMail(
+    {
+      from: EMAIL_FROM,
+      to: recipient,
+      subject: subject,
+      text: text_email,
+      html: html_email,
+    },
+    (err, info) => {
+      if (err) {
+        console.error("Error sending email");
+        res
+          .status(400)
+          .json({ success: false, message: "Error sending email" });
+      } else {
+        res
+          .status(200)
+          .json({ success: true, message: `Message ${info.messageId} sent` });
+      }
+    }
+  );
 });
 
 app.listen(PORT, () => {
