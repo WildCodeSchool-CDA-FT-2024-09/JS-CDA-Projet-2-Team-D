4152620355d2d546fe08a7028e3ecd926ca34d86
pjroggy <pierrejeanroggy@gmail.com>
2024-12-02 12:26:22 +0100

    add config test and add getBank test

--- api_data/src/index.ts
+++ api_data/src/index.ts
@@ -1,19 +1,9 @@
 import * as dotenv from "dotenv";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { buildSchema } from "type-graphql";
 import "reflect-metadata";
 import { AppDataSource } from "./db/data-source";
-import UserResolver from "./user/user.resolver";
-import CreditDebitResolver from "./creditDebit/creditDebit.resolver";
-import CategoryResolver from "./category/category.resolver";
-import SubcategoryResolver from "./subcategory/subcategory.resolver";
-import BankResolver from "./bank/bank.resolver";
-import BankAccountResolver from "./bankAccount/bank_account.resolver";
-import VatResolver from "./vat/vat.resolver";
-import StatusResolver from "./status/status.resolver";
-import CommissionResolver from "./commission/commission.resolver";
-import InvoiceResolver from "./invoice/invoice.resolver";
+import getSchema from "./schema";
 
 dotenv.config();
 const { PORT } = process.env;
@@ -21,21 +11,7 @@ const { PORT } = process.env;
 (async () => {
   await AppDataSource.initialize();
 
-  const schema = await buildSchema({
-    resolvers: [
-      UserResolver,
-      CreditDebitResolver,
-      CategoryResolver,
-      SubcategoryResolver,
-      BankResolver,
-      BankAccountResolver,
-      VatResolver,
-      StatusResolver,
-      CommissionResolver,
-      InvoiceResolver,
-    ],
-    validate: true,
-  });
+  const schema = await getSchema();
 
   const server = new ApolloServer({
     schema,
