b80309bd3ff3d3ecbbf8a8ba6bc2496013bcb1d8
Fabien Amann <fabien@feub.net>
2024-12-03 10:29:11 +0100

    feat/client: added basic router + MUI (font set to Montserrat)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,53 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <h1>Home page</h1>,
+      },
+      {
+        path: "administrator",
+        element: <Outlet />,
+        children: [
+          {
+            index: true,
+            element: <h1>Administrateur</h1>,
+          },
+        ],
+      },
+      {
+        path: "accountant",
+        element: <Outlet />,
+        children: [
+          {
+            index: true,
+            element: <h1>Comptable</h1>,
+          },
+        ],
+      },
+      {
+        path: "commission",
+        element: <Outlet />,
+        children: [
+          {
+            index: true,
+            element: <h1>Responsable de commission</h1>,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
