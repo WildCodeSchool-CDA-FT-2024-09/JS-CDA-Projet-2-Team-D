5904f972a1092994b1ede84c14884ef89478716b
Gadln <helene.gadessaud@icloud.com>
2024-12-14 17:47:48 +0100

    Fix feat/Invoice: delete yup

--- client/src/types/InvoiceInputType.ts
+++ client/src/types/InvoiceInputType.ts
@@ -1,33 +1,33 @@
-import * as Yup from "yup";
+// import * as Yup from "yup";
 
-export const invoiceValidationSchema = Yup.object({
-  commission_id: Yup.number()
-    .required("La commission est requise")
-    .min(1, "Sélectionnez une commission"),
-  date: Yup.date().required("La date est requise"),
-  category_id: Yup.number()
-    .required("La catégorie est requise")
-    .min(1, "Sélectionnez une catégorie"),
-  subcategory_id: Yup.number()
-    .required("La sous-catégorie est requise")
-    .min(1, "Sélectionnez une sous-catégorie"),
-  label: Yup.string()
-    .required("Le libellé est requis")
-    .max(50, "Le libellé ne doit pas dépasser 50 caractères")
-    .matches(
-      /^[A-Za-z0-9À-ÖØ-öø-ÿ@_\-\s]+$/,
-      "Le libellé ne peut contenir que des lettres, chiffres, espaces et certains caractères spéciaux",
-    ),
-  price_without_vat: Yup.number()
-    .required("Le prix HT est requis")
-    .min(0, "Le prix doit être positif ou nul"),
-  vat_id: Yup.number().required("Le taux de TVA est requis"),
-  receipt: Yup.mixed().required("Le justificatif est requis"),
-  info: Yup.string().max(
-    250,
-    "Les informations ne doivent pas dépasser 250 caractères",
-  ),
-});
+// export const invoiceValidationSchema = Yup.object({
+//   commission_id: Yup.number()
+//     .required("La commission est requise")
+//     .min(1, "Sélectionnez une commission"),
+//   date: Yup.date().required("La date est requise"),
+//   category_id: Yup.number()
+//     .required("La catégorie est requise")
+//     .min(1, "Sélectionnez une catégorie"),
+//   subcategory_id: Yup.number()
+//     .required("La sous-catégorie est requise")
+//     .min(1, "Sélectionnez une sous-catégorie"),
+//   label: Yup.string()
+//     .required("Le libellé est requis")
+//     .max(50, "Le libellé ne doit pas dépasser 50 caractères")
+//     .matches(
+//       /^[A-Za-z0-9À-ÖØ-öø-ÿ@_\-\s]+$/,
+//       "Le libellé ne peut contenir que des lettres, chiffres, espaces et certains caractères spéciaux",
+//     ),
+//   price_without_vat: Yup.number()
+//     .required("Le prix HT est requis")
+//     .min(0, "Le prix doit être positif ou nul"),
+//   vat_id: Yup.number().required("Le taux de TVA est requis"),
+//   receipt: Yup.mixed().required("Le justificatif est requis"),
+//   info: Yup.string().max(
+//     250,
+//     "Les informations ne doivent pas dépasser 250 caractères",
+//   ),
+// });
 
 export interface InvoiceState {
   commission_id: number;
