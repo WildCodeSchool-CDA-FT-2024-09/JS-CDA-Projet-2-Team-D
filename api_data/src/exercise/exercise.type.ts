10fa4611718b17252261f66317ec028e0d5f80f2
Fabien Amann <fabien@feub.net>
2024-12-30 14:35:25 +0100

    feat/api_data: added mutation CreateNewExercise

--- api_data/src/exercise/exercise.type.ts
+++ api_data/src/exercise/exercise.type.ts
@@ -1,6 +1,30 @@
+import { InputType, Field } from "type-graphql";
+import { IsString, IsNotEmpty, Length } from "class-validator";
+
 export type Exercise = {
   id: number;
   label: string;
   start_date: Date;
   end_date: Date;
 };
+
+@InputType()
+export class ExerciseInput {
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 100)
+  label: string;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 50)
+  start_date: Date;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 50)
+  end_date: Date;
+}
