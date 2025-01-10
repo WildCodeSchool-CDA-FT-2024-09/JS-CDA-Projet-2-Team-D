a17dfd8d3f469d2c8178387abed09e466b0d554c
Fabien Amann <fabien@feub.net>
2025-01-10 15:01:09 +0100

    refactor/api_email: added route for password email

--- api_email/src/index.ts
+++ api_email/src/index.ts
@@ -20,7 +20,7 @@ app.get("/", (_, res: Response) => {
   res.send("Hello Email!");
 });
 
-app.post("/send-email", async (req: Request, res: Response) => {
+app.post("/send-email-invoice", async (req: Request, res: Response) => {
   const { recipient, subject, fullname, invoiceNumber } = req.body;
 
   // Data for the template
@@ -28,7 +28,41 @@ app.post("/send-email", async (req: Request, res: Response) => {
 
   // Render the email template
   const emailHtml = await ejs.renderFile(
-    path.join(__dirname, "views", "email-template.ejs"),
+    path.join(__dirname, "views", "email-template-invoice.ejs"),
+    templateData
+  );
+
+  transporter.sendMail(
+    {
+      from: EMAIL_FROM,
+      to: recipient,
+      subject: subject,
+      html: emailHtml,
+    },
+    (err, info) => {
+      if (err) {
+        console.error("Error sending email");
+        res
+          .status(400)
+          .json({ success: false, message: "Error sending email" });
+      } else {
+        res
+          .status(200)
+          .json({ success: true, message: `Message ${info.messageId} sent` });
+      }
+    }
+  );
+});
+
+app.post("/send-email-password", async (req: Request, res: Response) => {
+  const { recipient, subject, fullname, email, password } = req.body;
+
+  // Data for the template
+  const templateData = { fullname, email, password };
+
+  // Render the email template
+  const emailHtml = await ejs.renderFile(
+    path.join(__dirname, "views", "email-template-password.ejs"),
     templateData
   );
 
