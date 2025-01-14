08082b1c690e4e0a5e7f8d3cb642fe15184847db
Fabien Amann <fabien@feub.net>
2025-01-14 17:02:28 +0100

    refactor/api_email: not auth on reset password

--- api_email/src/index.ts
+++ api_email/src/index.ts
@@ -147,43 +147,39 @@ app.post(
   }
 );
 
-app.post(
-  "/send-reset-password",
-  authenticatedRoute,
-  async (req: AuthenticatedRequest, res: Response) => {
-    const { recipient, subject, resetUrl } = req.body;
+app.post("/send-reset-password", async (req: Request, res: Response) => {
+  const { recipient, subject, resetUrl } = req.body;
 
-    // Data for the template
-    const templateData = { resetUrl };
+  // Data for the template
+  const templateData = { resetUrl };
 
-    // Render the email template
-    const emailHtml = await ejs.renderFile(
-      path.join(__dirname, "views", "email-template-reset-password.ejs"),
-      templateData
-    );
+  // Render the email template
+  const emailHtml = await ejs.renderFile(
+    path.join(__dirname, "views", "email-template-reset-password.ejs"),
+    templateData
+  );
 
-    transporter.sendMail(
-      {
-        from: EMAIL_FROM,
-        to: recipient,
-        subject: subject,
-        html: emailHtml,
-      },
-      (err, info) => {
-        if (err) {
-          console.error("Error sending email");
-          res
-            .status(400)
-            .json({ success: false, message: "Error sending email" });
-        } else {
-          res
-            .status(200)
-            .json({ success: true, message: `Message ${info.messageId} sent` });
-        }
+  transporter.sendMail(
+    {
+      from: EMAIL_FROM,
+      to: recipient,
+      subject: subject,
+      html: emailHtml,
+    },
+    (err, info) => {
+      if (err) {
+        console.error("Error sending email");
+        res
+          .status(400)
+          .json({ success: false, message: "Error sending email" });
+      } else {
+        res
+          .status(200)
+          .json({ success: true, message: `Message ${info.messageId} sent` });
       }
-    );
-  }
-);
+    }
+  );
+});
 
 app.listen(PORT, () => {
   console.info(`🚀 API email server ready at http://localhost:${PORT}`);
