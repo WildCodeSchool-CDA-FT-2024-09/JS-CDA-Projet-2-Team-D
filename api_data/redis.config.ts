82ac5f84b6aa2e500dfcd415931fd1df5e21861a
Fabien Amann <fabien@feub.net>
2025-01-16 15:51:00 +0100

    chore/api_data: comments cleanup

--- api_data/redis.config.ts
+++ api_data/redis.config.ts
@@ -6,11 +6,10 @@ const { REDIS_HOST, REDIS_PORT } = process.env;
 
 const redisClient = createClient({
   url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
-}); // * 1
+});
 
 redisClient.on("error", (err) => {
-  // * 2
-  console.info("Redis Client Error", err);
+  console.error("Redis Client Error", err);
 });
 redisClient.on("connect", () => {
   console.info("redis connected");
