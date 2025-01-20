6fa75b3fff3a2308e06ca5d4606ec5a1f51398f6
Fabien Amann <fabien@feub.net>
2025-01-20 10:50:54 +0100

    refactor: redis conf in docker compose

--- E2E/tests/login.spec.ts
+++ E2E/tests/login.spec.ts
@@ -3,8 +3,10 @@ import { test, expect } from "@playwright/test";
 test("connect as Super Admin", async ({ page }) => {
   await page.goto("https://predeploy.students-cda-js-4.wilders.dev/");
 
-  await page.getByLabel("Adresse Email *").fill("super@admin.net");
-  await page.getByLabel("Mot de passe", { exact: true }).fill("whS0@cqnuros");
+  // await page.getByLabel("Adresse Email *").fill("super@admin.net");
+  await page.fill("#email", "super@admin.net");
+  // await page.getByLabel("Mot de passe", { exact: true }).fill("whS0@cqnuros");
+  await page.fill("#password", "whS0@cqnuros");
 
   await page.on("request", (request) => {
     console.log("Requête interceptée :", request.method(), request.url());
