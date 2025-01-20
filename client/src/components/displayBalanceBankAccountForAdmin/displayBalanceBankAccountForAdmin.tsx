cbceb7e27ab96463eb3f98fe6acd46139614a52c
Gadln <helene.gadessaud@icloud.com>
2025-01-20 11:15:00 +0100

    feat/BankAccount: Fixed CSS and added component on HomePageAdministrator

--- client/src/components/displayBalanceBankAccountForAdmin/displayBalanceBankAccountForAdmin.tsx
+++ client/src/components/displayBalanceBankAccountForAdmin/displayBalanceBankAccountForAdmin.tsx
@@ -1,9 +1,10 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { BarChart } from "@mui/x-charts/BarChart";
-import { useGetBanksQuery } from "../../types/graphql-types";
 import { useMediaQuery, useTheme } from "@mui/material";
+// import { axisClasses } from '@mui/x-charts';
 import { colors } from "../../utils/chartColors";
+import { useGetBanksQuery } from "../../types/graphql-types";
 
 interface BankAccount {
   __typename?: "BankAccount";
@@ -23,6 +24,7 @@ interface Bank {
 export function DisplayBalanceBankAccountForAdmin() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
   const { data, loading, error } = useGetBanksQuery();
 
   if (loading) return <p>Chargement...</p>;
@@ -38,68 +40,83 @@ export function DisplayBalanceBankAccountForAdmin() {
   const banks = (data?.getBanks || []) as Bank[];
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: isMobile ? "column" : "row",
-        gap: 4,
-        justifyContent: "center",
-      }}
-    >
-      {banks.map((bank) => {
-        const bankAccounts = bank.bankAccounts || [];
+    <Box>
+      <Typography variant="h2" align="center" gutterBottom sx={{ mb: 4 }}>
+        État des soldes bancaires par banque
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: isMobile ? "column" : "row",
+          gap: 4,
+          justifyContent: "center",
+        }}
+      >
+        {banks.map((bank) => {
+          const bankAccounts = bank.bankAccounts || [];
 
-        const series = [
-          {
-            data: bankAccounts.map((account) => account.balance),
-            label: "Solde",
-          },
-        ];
+          const series = bankAccounts.map((account) => ({
+            data: [account.balance],
+            label: account.name,
+            valueFormatter: (value: number | null) =>
+              value !== null
+                ? new Intl.NumberFormat("fr-FR", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(value)
+                : "",
+          }));
 
-        const categories = bankAccounts.map((account) => account.name);
-
-        return (
-          <Box
-            key={bank.id}
-            sx={{
-              flex: isMobile ? "1 1 100%" : "1 1 45%",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              gap: 2,
-            }}
-          >
-            <Typography variant="h5" gutterBottom>
-              {bank.label}
-            </Typography>
-            <BarChart
-              height={isMobile ? 400 : 300}
-              series={series}
-              colors={bankAccounts.map(
-                (_, accountIndex) => colors[accountIndex % colors.length],
-              )}
-              xAxis={[
-                {
-                  data: categories,
-                  scaleType: "band",
-                },
-              ]}
+          return (
+            <Box
+              key={bank.id}
               sx={{
-                "& .MuiLegend-root": {
-                  marginTop: "16px",
-                  display: "flex",
-                  flexWrap: "wrap",
-                  justifyContent: "center",
-                  gap: "50px",
-                },
-                "& .MuiLegend-item": {
-                  marginBottom: "8px",
-                },
+                flex: isMobile ? "1 1 100%" : "1 1 45%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: 2,
               }}
-            />
-          </Box>
-        );
-      })}
+            >
+              <Typography variant="h5" gutterBottom>
+                {bank.label}
+              </Typography>
+              <BarChart
+                height={isMobile ? 400 : 300}
+                margin={{
+                  top: 20,
+                  right: 20,
+                  bottom: 90, // Augmenté pour la légende
+                  left: 40,
+                }}
+                series={series}
+                colors={bankAccounts.map(
+                  (_, accountIndex) => colors[accountIndex % colors.length],
+                )}
+                xAxis={[
+                  {
+                    data: [""],
+                    scaleType: "band",
+                  },
+                ]}
+                slotProps={{
+                  legend: {
+                    direction: "row",
+                    position: {
+                      vertical: "bottom",
+                      horizontal: "middle",
+                    },
+                    padding: { top: 20 },
+                    hidden: false,
+                  },
+                }}
+              />
+            </Box>
+          );
+        })}
+      </Box>
     </Box>
   );
 }
+
+export default DisplayBalanceBankAccountForAdmin;
